ffb263cda7ed41f96b11b389eda4799991c8bb09
LCUKP <puwanatjaijitjaam@gmail.com>
2025-07-25 11:42:40 +0700

    q

---
+++ 00-Quiz/01-GDB_Lab_01/08.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+int main() {
+    int a = 17, b = 25, c = 50, d = 10;
+
+    // Assignment Statements of Each Question
+    d = a++ + b++ + c++;
+    d = ++a + ++b + ++c;
+    return 0;
+}
